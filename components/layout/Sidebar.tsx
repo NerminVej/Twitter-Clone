b10b9478ea3cb768c84874db6a34ea384229a9f8
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-28 14:49:09 +0200

    added the Sidebar to the project

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,50 @@
+import { BiLogOut } from 'react-icons/bi';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+
+
+const Sidebar = () => {
+
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/',
+    },
+    {
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+    },
+    {
+      icon: FaUser,
+      label: 'Profile',
+      auth: true,
+    },
+  ]
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col items-end">
+          <div className="space-y-2 lg:w-[230px]">
+            <SidebarLogo />
+            {items.map((item) => (
+              <SidebarItem
+                key={item.href}
+                alert={item.alert}
+                auth={item.auth}
+                href={item.href} 
+                icon={item.icon} 
+                label={item.label}
+              />
+            ))}
+            
+          </div>
+        </div>
+      </div>
+  )
+};
+
+export default Sidebar;
