fcb743cbdb6eed5dfb8b2b21d3c383f3f038d365
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-28 14:46:04 +0200

    added the layout without the sidebar and followerbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Layout = () => {
+
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div
+            className="
+              col-span-3 
+              lg:col-span-2 
+              border-x-[1px] 
+              border-neutral-800
+          "
+          >
+            {children}
+          </div>
+          <FollowBar />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
