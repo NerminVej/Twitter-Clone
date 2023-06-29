f4489fa55f3fe6e1094de38ce0d65beddc5d3f0c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-29 12:48:33 +0200

    added styling to the button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,10 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return;
+  <>
+  <Modal />
   <Layout>
     <Component {...pageProps} />
   </Layout>;
+  </>
 }
