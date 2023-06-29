9b077a1c8e98d0999f0a765f54f04ff1b71ace3f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-29 12:51:46 +0200

    added the Input component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import Layout from "@/components/Layout";
-import { Modal } from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return;
   <>
-  <Modal />
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>;
-  </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    ;
+  </>;
 }
